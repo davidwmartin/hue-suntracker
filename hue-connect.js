15ca6698871a87a958a7245c482db2f5608342b3
David Martin <david.w.martin@me.com>
2017-03-17 12:53:52 -0700

    functioning hue-connect script

--- hue-connect.js
+++ hue-connect.js
@@ -8,54 +8,48 @@ var hue = require('node-hue-api'),
 // setup bridge connection vars
 // NOTE -- still requires manually setting the username (stored in config/hue-login.js so I don't have to publish my config to github)
 var username = hueLogin.username,
-		hostname;
+		hostname,
+		api;
 
 
 ////////////// FIRE MISSILES
 // NOTE -- meant to be called from command line (i.e. w/cron), assumes first argument will be the ID of the desired scene
 var requestedSceneId = process.argv[2];
-boomBoom(requestedSceneId);
+hueActivate(requestedSceneId);
 
 
+////// Guts of it
 
-
-////// Main function to run all the below
-// ((EXPORT THIS!!!)) not right now...
-function boomBoom(sceneId){
+// Main function -- connects to bridge and activates scene (scene ID passed in as argument)
+function hueActivate(sceneId){
 	// search for bridges
 	hue.nupnpSearch()
-		.then(getBridgeHost)
-		// .then(bridgeConnect)
-		.then(changeScene(sceneId))
+		.then(function(data){
+			
+			// TODO -- feel like this could be cleaner
+			hostname = bridgeConnect(data);
+			changeScene(sceneId, hostname);
+
+		})
 		.done();
-};
+}
 
-///// search network for bridges
-// hue.nupnpSearch().then(getBridgeHost).done();
-
-// gets IP address of first bridge connected to network
-function getBridgeHost(bridge){
+// gets IP address of first bridge connected to network, make api connection
+function bridgeConnect(bridge){
 	hostname = bridge[0].ipaddress;
 	console.log("bridge host: " + hostname);
-
-};
-
-// // Connects to bridge w/completed creds
-// function bridgeConnect(){
-// 	console.log('connect success');
-
-// 	// api.getFullState().then(displayResult).done();
-// };
+	api = new HueApi(hostname, username)
+	console.log(api);
+	return api;
+}
 
 // Changes light scene based on passed in scene ID
-function changeScene(sceneId){
-	var api = new HueApi(hostname, username);
+function changeScene(sceneId, api){
 	api.activateScene(sceneId)
-		.then(console.log('changed scene to ' + sceneId))
-		.then(console.log('api: ' + api.hostname));
-};
+		.then(console.log('changed scene to ' + sceneId));
+}
 
 // Utility function to display result of bridge calls
 displayResult = function(result) {
     console.log(JSON.stringify(result, null, 2));
-};
+}
