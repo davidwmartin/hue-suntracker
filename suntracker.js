5bfe48312f441bfaf9a9a6e1a0e8892428ecb708
David Martin <david.w.martin@me.com>
2017-03-18 16:27:36 -0700

    refactored code to address bug in moment creation -- now appears to be fully functioning

--- suntracker.js
+++ suntracker.js
@@ -2,10 +2,8 @@
 var querystring = require('querystring'),
 		http = require('http'),
 		crontab = require('crontab'), 
-		moment = require('moment');
-
-// light event variables (may not need these here)
-var lightEvents = [];
+		moment = require('moment'),
+		lightEvents = require('./config/sunset-events'); // gets lightEvents array from external config file
 
 
 ////// Main Action Here:
@@ -50,68 +48,45 @@ function calcLightEvents(sunDataSet){
 
 	console.log("sunset: " + sunDataSet);
 
-	// get moment.js moment for sunset
-	var sdrMoment = moment(sunDataSet);
-	// get moments for various offsets from sunset
-	var prePreSunset = sdrMoment.subtract(120,'m').format("m H D M");
-	var preSunset = sdrMoment.subtract(45,'m').format("m H D M");
-	var postSunset1 = sdrMoment.add(75,'m').format("m H D M");
-	var postSunset2 = sdrMoment.add(255,'m').format("m H D M");
+	console.log("events:");
+	for (i = 0; i < lightEvents.length; i++){
+		// get moment.js moment for sunset
+		var ssMoment = moment(sunDataSet);
 
-	// TODO - seems a bit verbose setting up the event array this way (also some dirrrttyy ish happening with the string concatenation) (also not super easy to add events if doing it this way)
-	// TODO - unnecessary
-	firstEventTime = preSunset + " *";
-	secondEventTime = postSunset1 + " *";
-	thirdEventTime = postSunset2 + " *";
-	fourthEventTime = prePreSunset + " *";
+		// get each event's timing info by adding its offset to the sunset moment
+		lightEvents[i].moment = ssMoment.add(lightEvents[i].offset, "m");
+		console.log(JSON.stringify(lightEvents[i]));
 
-	// timing variables and array of timeXlight events
-	lightEvents = [
-		{
-			time:fourthEventTime,
-			scene:"SzTWcta-JSvvPj1" // "creekbed afternoon"
-		},
-		{
-			time:firstEventTime,
-			scene:"eL1TQfrb8mmUfjS" // "bright"
-		},
-		{
-			time:secondEventTime,
-			scene:"PVB-BYTBE2OzN7j" // "pencils"
-		},
-		{
-			time:thirdEventTime,
-			scene:"t6BAS0g6mBJiznk" // "tropical twilight"
-		}
-	];
+	}
 
-	console.log('calculated events: '+ lightEvents.length);
-
-	// return lightEvents;
 	cronSched(lightEvents);
 }
 
 
 ////// CRON
-
 // Gets path of hue script (for cron command)
-// TODO -- move / refactor?
 function getHueScriptPath(){
 	var lightScriptPath = require.resolve('./hue-connect');
 	return lightScriptPath;
 }
 
+// Formats cron time
+function formatCronTime(jobMoment){
+	var formattedTime = jobMoment.format("m H D M") + " *";
+	console.log("formatted time: " + formattedTime);
+	return formattedTime;
+}
+
 // formats cron command
-function formatCron(sceneId){
+function formatCronCommand(sceneId){
 	var hueScriptPath = getHueScriptPath();
 	// TODO -- better way of formatting command? bit rough, but I suppose it works
 	formattedCommand = "node " + hueScriptPath + " " + sceneId;
 	console.log("formatted command: " + formattedCommand);
-
 	return formattedCommand;
 }
 
-// schedules cron jobs
+// schedules cron jobs (takes array of light events which have moment properies added)
 function cronSched(lightEvents){
 
 	crontab.load(function(err, crontab) {
@@ -124,8 +99,9 @@ function cronSched(lightEvents){
 		for (i = 0; i < lightEvents.length; i++){
 
 			jobScene = lightEvents[i].scene;
-			jobCommand = formatCron(jobScene);
-			jobTime = lightEvents[i].time;
+			jobMoment = lightEvents[i].moment;
+			jobTime = formatCronTime(jobMoment);
+			jobCommand = formatCronCommand(jobScene);
 
 			// var currentJob = crontab.create(jobCommand,jobTime);
 			var currentJob = crontab.create(jobCommand, jobTime);
@@ -134,13 +110,10 @@ function cronSched(lightEvents){
 
 		}
 
-		var jobsss = crontab.jobs();
-		console.log(jobsss);
 		// save
 	  crontab.save(function(err, crontab) {});
 
 		console.log("cronSched complete");
-		console.log("crontab: " + JSON.stringify(crontab));
 
 	});
 
